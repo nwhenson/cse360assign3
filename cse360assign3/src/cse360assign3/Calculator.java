df79a935e1f59184fc9e25c553a84a2829a2eef4
Nick Henson <nwhenson@asu.edu>
2016-02-22 02:07:06 -0800

    3rd commit to Git

--- cse360assign3/src/cse360assign3/Calculator.java
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -2,42 +2,76 @@ package cse360assign3;
 /**
  * This class is a programmed calculator that will add, subtract, multiply, divide, and get the history
  * of the inputs
- * @author Nick Henson
+ * @author Nick Henson Pin: 405
  * @version 2/21/16
+ * Github URL: https://github.com/nwhenson/cse360assign3.git
  */
 
 public class Calculator {
-
+	
+	/**	This private int represents the total value after each operation */
 	private int total;
+	
+	/**	This private String represents the operation history that total has undergone */
+	private String history = "0";
 	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	/**
+	 * getTotal() will return the value of int total
+	 * @return total
+	 */
 	public int getTotal () {
 		return total;
 	}
 	
+	/**
+	 * add() will add the parameter value to int total
+	 * @param value
+	 */
 	public void add (int value) {
 		total = total + value;
+		history += " + " + value;
 	}
 	
+	/**
+	 * subtract() will subtract the parameter value from int total
+	 * @param value
+	 */
 	public void subtract (int value) {
 		total = total - value;
+		history += " - " + value;
 	}
 	
+	/**
+	 * multiply() will multiply the parameter value to int total
+	 * @param value
+	 */
 	public void multiply (int value) {
 		total = total * value;
+		history += " * " + value;
 	}
 	
+	/**
+	 * divide() will divide the parameter value from int total
+	 * @param value
+	 */
 	public void divide (int value) {
 		if (value == 0)
 			total = 0;
 		else if (value != 0)
 			total = total / value;
+		
+		history += " / " + value;
 	}
 	
+	/**
+	 * getHistory() will return String history
+	 * @return history
+	 */
 	public String getHistory () {
-		return "";
+		return history;
 	}
 }
--- cse360assign3/src/cse360assign3/CalculatorTest.java
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -1,4 +1,10 @@
 package cse360assign3;
+/**
+ * This contains test cases for all the methods in Calculator
+ * @author Nick Henson
+ * @version 2/22/16
+ * Github URL: https://github.com/nwhenson/cse360assign3.git
+ */
 
 import static org.junit.Assert.*;
 
@@ -6,13 +12,18 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
+	/**
+	 * testCalculator() will make sure the constructor is not null
+	 */
 	@Test
 	public void testCalculator() {
 		Calculator total = new Calculator();
-		if (total == null)
-			fail("Constructor is null");
+		assertNotNull(total);
 	}
 
+	/**
+	 * testGetTotal() will make sure getTotal() has the proper return value
+	 */
 	@Test
 	public void testGetTotal() {
 		Calculator total = new Calculator();
@@ -23,6 +34,9 @@ public class CalculatorTest {
 		assertEquals(total.getTotal(), 0);
 	}
 
+	/**
+	 * testAdd() will make sure the add() function works properly
+	 */
 	@Test
 	public void testAdd() {
 		Calculator total = new Calculator();
@@ -34,6 +48,9 @@ public class CalculatorTest {
 		assertEquals(total.getTotal(), 27);
 	}
 
+	/**
+	 * testSubtract() will make sure the subtract() function works properly
+	 */
 	@Test
 	public void testSubtract() {
 		Calculator total = new Calculator();
@@ -47,6 +64,9 @@ public class CalculatorTest {
 		assertEquals(total.getTotal(), 1);
 	}
 
+	/**
+	 * testMultiply() will make sure the multiply() function works properly
+	 */
 	@Test
 	public void testMultiply() {
 		Calculator total = new Calculator();
@@ -62,6 +82,9 @@ public class CalculatorTest {
 		assertEquals(total.getTotal(), 0);
 	}
 
+	/**
+	 * testDivide() will make sure the divide() function works properly
+	 */
 	@Test
 	public void testDivide() {
 		Calculator total = new Calculator();
@@ -78,9 +101,17 @@ public class CalculatorTest {
 		assertEquals(total.getTotal(), 0);
 	}
 
+	/**
+	 * testGetHistory() will make sure the getHistory() returns the proper String
+	 */
 	@Test
 	public void testGetHistory() {
-		fail("Not yet implemented");
+		Calculator total = new Calculator();
+		total.add(3);
+		total.multiply(5);
+		total.subtract(2);
+		total.divide(0);
+		assertEquals(total.getHistory(), "0 + 3 + 5 + 2 + 0");
 	}
 
 }
